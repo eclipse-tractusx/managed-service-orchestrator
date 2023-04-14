b9364755d52cf4a626d9a64e52095837d91629ef
Sachin Argade <sachin.argade@t-systems.com>
2023-04-14 11:40:14 +0530

    corrected copyright header

--- src/main/java/org/eclipse/tractusx/autosetup/manager/DTRegistryManager.java
+++ src/main/java/org/eclipse/tractusx/autosetup/manager/DTRegistryManager.java
@@ -21,26 +21,6 @@
 package org.eclipse.tractusx.autosetup.manager;
 
 import java.util.HashMap;
-/********************************************************************************
- * Copyright (c) 2023 T-Systems International GmbH
- * Copyright (c) 2023 Contributors to the Eclipse Foundation
- *
- * See the NOTICE file(s) distributed with this work for additional
- * information regarding copyright ownership.
- *
- * This program and the accompanying materials are made available under the
- * terms of the Apache License, Version 2.0 which is available at
- * https://www.apache.org/licenses/LICENSE-2.0.
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
- *
- * SPDX-License-Identifier: Apache-2.0
- ********************************************************************************/
-
 import java.util.Map;
 import java.util.UUID;
 
@@ -98,8 +78,9 @@ public class DTRegistryManager {
 			inputData.put("dtregistryUrlPrefix", sDEConfigurationProperty.getDtregistryUrlPrefix());
 
 			outputData.put("sde.digital-twins.hostname", dtregistryUrl);
-			outputData.put("sde.digital-twins.authentication.url", sDEConfigurationProperty.getDigitalTwinsAuthenticationUrl());
-			
+			outputData.put("sde.digital-twins.authentication.url",
+					sDEConfigurationProperty.getDigitalTwinsAuthenticationUrl());
+
 			if (AppActions.CREATE.equals(action))
 				appManagement.createPackage(DT_REGISTRY, packageName, inputData);
 			else
